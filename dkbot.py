7a83fdff548028e1a2e2495a88cf4745ed8336c3
cohama <hirotaka.hamada.1024@gmail.com>
2010-12-14 01:26:48 +0900

    日付を datetime ではなく、月と日で別々に保存するように変更。

--- dkbot.py
+++ dkbot.py
@@ -1,5 +1,4 @@
-﻿
-import re
+﻿import re
 from datetime import datetime, timedelta
 from google.appengine.ext import webapp
 from google.appengine.ext import db
@@ -10,7 +9,8 @@ from index import doRender
 class Schedule(db.Model):
 	id = db.IntegerProperty()
 	user = db.StringProperty()
-	date = db.DateTimeProperty()
+	day = db.IntegerProperty()
+	month = db.IntegerProperty()
 	todo = db.StringProperty()
 
 
@@ -25,37 +25,26 @@ class DateKeeperBotHandler(webapp.RequestHandler):
 	def _get_schedule_from_tweet(self):
 		tweet_list = self.api.user_timeline(screen_name='c0hama')
 		
-		now_year = datetime.now().year
-
 		for tweet in tweet_list:
-			tweet.created_at = self._to_Tokyo_timezone(tweet.created_at)
-			
-			sch = self._create_schedule(tweet)
-			if (sch != None):
-				if (self._is_new_schedule(sch)):
+			if self._is_new_tweet(tweet):
+				sch = self._create_schedule(tweet)
+				if sch != None:
 					sch.put()
-					
-		
-	def _to_Tokyo_timezone(self, date):
-		return date + timedelta(hours=9)
-		
+	
 	def _create_schedule(self, tweet):
 		mo = re.match(ur'(\d+)月(\d+)日は(.*)', tweet.text)
 		
 		if mo:
-		
 			month = int(mo.group(1))
 			day = int(mo.group(2))
 			text = mo.group(3)
-			posted_date = datetime(year=datetime.now().year, month=month, day=day, hour=8)
 			
-			return Schedule(id=tweet.id, user=tweet.user.screen_name, date=posted_date, todo=text)
+			return Schedule(id=tweet.id, user=tweet.user.screen_name, day=day, month=month, todo=text)
 			
 		else:
-		
 			return None
 	
-	def _is_new_schedule(self, schedule):
+	def _is_new_tweet(self, schedule):
 		query = Schedule.all()
 		query.filter('id =', schedule.id)
 		return query.count() == 0
